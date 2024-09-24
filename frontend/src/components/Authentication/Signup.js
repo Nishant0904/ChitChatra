895f8010ccdfebe0db21f1f5fc509611571a1637
Nishant <nishantg.it.22@nitj.ac.in>
2024-09-24 21:55:30 +0530

    first commit

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -100,9 +100,9 @@ const Signup = () => {
     if (pics.type === "image/jpeg" || pics.type === "image/png") {
       const data = new FormData();
       data.append("file", pics);
-      data.append("upload_preset", "chat-app");
-      data.append("cloud_name", "piyushproj");
-      fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
+      data.append("upload_preset", "chit-chatra");
+      data.append("cloud_name", "dmc8lpgzq");
+      fetch("https://api.cloudinary.com/v1_1/dmc8lpgzq/image/upload", {
         method: "post",
         body: data,
       })
